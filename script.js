d5cd89deedbf1e4aacfa4c9ab72cb1ac9805b349
JSamRojas <jonas23450947@gmail.com>
2025-01-31 23:15:14 -0600

    intento final

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 btnCloseModal.addEventListener('click', closeModal);
